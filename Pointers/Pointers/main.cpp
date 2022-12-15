99d7a7196fe8b63d1ec6acbd09a9bc413a00f7b1
Clayman <Clayman@DESKTOP-DM8E77U>
2022-12-15 18:22:18 +0200

    Pointers arithmetic

---
+++ Pointers/Pointers/main.cpp
@@ -0,0 +1,61 @@
+﻿#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+//#define POINTERS_BASICS
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef POINTERS_BASICS
+	int a = 2;
+	int* pa = &a;		//Pointer to 'a'
+	cout << a << endl;	//значение переменной 'a'
+	cout << &a << endl;	//взятие адреас переменной 'a' прямо при выводе
+	cout << pa << endl;	//вывод адреса переменной 'a', хранящегося в указателе 'pa'
+	cout << *pa << endl;//разыменование указателя 'pa' и вывод на экран значения по адресу (переменной 'a')
+
+	int* pb;	//просто объявление укзателя (без инициализации)
+	int b = 3;
+	pb = &b;	//инициализируем указатель после объявления
+
+	//int - int
+	//int* - указатель на int
+	//double - double
+	//double* - указатель на double  
+#endif // POINTERS_BASICS
+
+	const int n = 5;
+	int arr[n] = { 3,5,8,13,21 };
+	cout << arr << endl;
+	cout << *arr << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(arr + i) << "\t";
+	}
+	cout << endl;
+}
+
+/*
+----------------------------------------------------
+& - Address-of operator  (Оператор взятия адреса)
+* - Dereference operator (Оператор разыменования)
+----------------------------------------------------
+*/
+
+/*
+----------------------------------------------------
+			Pointers arithmetic:
++
+-
+++
+--
+
+char*  + 1B = 1B;
+short* + 1B = 2B;
+int*   + 1B = 4B;
+double*+ 1B = 8B;
+----------------------------------------------------
+*/
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,6 +1,9 @@
 https://github.com/okovtun/BV_225.git
 https://www.youtube.com/watch?v=owJLioIXPds&list=PLeqyOOqxeiIOn6FFT2a2xBVF9UpViJ-WH
 
+TODO:
+Выучить теорию по указателям.
+
 TOREAD:
 https://www.softelectro.ru/ieee754.html
 https://www.ixbt.com/cpu/x86-cpu-faq-2006.shtml
